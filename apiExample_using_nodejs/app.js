9591c93be7141fae31aafcf2c1ac1a3b576ea555
Snigdha <snigdha@gmail.com>
2018-07-04 12:27:32 +0530

    revised files

---
+++ apiExample_using_nodejs/app.js
@@ -0,0 +1,26 @@
+var express= require("express");
+var bodyParser= require("body-parser");
+var app=express();
+var port=9000;
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
+app.get('/',(req,res)=>
+{
+    res.send("hello world !!");
+});
+app.get('/user', function(req, res) {
+    res.sendFile(__dirname + "/" + "main.html");
+});
+app.post('/abc', function(req, res)
+{
+    r = {
+        first : req.body.first,
+        last : req.body.last
+    };
+    console.log(r);
+    res.end(JSON.stringify(r));
+});
+app.listen(port,()=>
+{
+    console.log("server started!!! at port "+ port);
+});
---
+++ sample_of_CURD_using_angular&nodejs/server/app.js
@@ -0,0 +1,32 @@
+var express = require('express');
+var path = require('path');
+var bodyParser = require('body-parser');
+//var Request = require("request");
+var insertController=require('./controller/insert');
+var updateController=require('./controller/update');
+var updateuserController=require('./controller/updateuser');
+var viewuserController=require('./controller/viewinfo');
+var loginController=require('./controller/login');
+var registerController=require('./controller/register');
+var app = express();
+var port=9000;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'dist')));
+
+app.post('/api/insert',insertController.register);
+app.post('/api/update',updateController.authenticate);
+app.post('/api/updateuser',updateuserController.update_user);
+app.post('/api/viewinfo',viewuserController.view_user);
+app.post('/api/login',loginController.login_user);
+app.post('/api/register',registerController.register);
+
+app.get('*', function(req, res) {
+        res.sendFile(path.join(__dirname, 'dist/index.html'));
+});
+
+console.log("server is running on port "+port);
+app.listen(9000);
+module.exports = app;
+
